033a2cf7250b7e4f6ffbd71f83a791548ec93f08
edoshinsm <sergey.edoshin@gmail.com>
2026-07-02 02:33:20 +0300

    д/з-4 fix

--- exercise-4/src/main/java/myapp/Main.java
+++ exercise-4/src/main/java/myapp/Main.java
@@ -15,24 +15,4 @@ public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-
-    @Bean
-    CommandLineRunner testRunner(FileManagerService fileService) {
-        return args -> {
-            System.out.println("--- ЗАПУСК ТЕСТА ---");
-
-            var directoryContent = fileService.getDirContent("C:\\Users\\serge\\IdeaProjects\\edu-stud-java-spring");
-
-            System.out.println("--- Директории ---");
-            for(var dir : directoryContent.getDirectoryMetadataList()) {
-                System.out.println(dir.name());
-                Path parentPath = Paths.get(dir.path()).toAbsolutePath();
-                System.out.println(parentPath);
-            }
-            System.out.println("--- Файлы ---");
-            for(var file : directoryContent.getFileMetadataList()) {
-                System.out.println(file.name() + " " + " " + file.size() + " " + file.lastModified());
-            }
-        };
-    }
 }
